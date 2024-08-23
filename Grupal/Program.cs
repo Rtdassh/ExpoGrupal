e48d71cdc2db7ccee24aea0a3328555895d5e896
diegojao <diegoalegriaoros1@gmail.com>
2024-08-22 21:09:47 -0600

    Menus y submenus

--- Grupal/Program.cs
+++ Grupal/Program.cs
@@ -4,7 +4,7 @@ do
 {
     try
     {
-        LecturaSwitch();
+        Menu();
     }
     catch (FormatException ex)
     {
@@ -13,38 +13,99 @@ do
     }
 } while (menu);
 
-int Menu()
+
+void Menu()
+{
+    while (menu)
+    {
+        Console.Clear();
+        Console.WriteLine("_____________________________________________________");
+        Console.WriteLine("             Hospital Pediatrico Alegría");
+        Console.WriteLine("¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯");
+        Console.WriteLine("1. Agregar persona");
+        Console.WriteLine("2. Programar cita");
+        Console.WriteLine("3. Consultar listados");
+        Console.WriteLine("4. Agregar tratamiento");
+        Console.WriteLine("5. Salir");
+        Console.Write("Ingrese una opción del menú: ");
+        int optionSwitch = Convert.ToInt32(Console.ReadLine());
+        switch (optionSwitch)
+        {
+            case 1:
+                agregarPersona();
+                break;
+            case 2:
+                //programarCita();
+                break;
+            case 3:
+                consultarListado();
+                break;
+            case 4:
+                //agregarTratamiento();
+                break;
+            case 5:
+                menu = false; break;
+            default:
+                Console.WriteLine("Ingrese una opción valida"); Console.ReadKey();
+                break;
+        }
+    }
+}
+void agregarPersona()
 {
     Console.Clear();
-    Console.WriteLine("Hospital Pediatrico Alegría");
-    Console.WriteLine("1. ");
-    Console.WriteLine("2. ");
-    Console.WriteLine("3. ");
-    Console.WriteLine("4. ");
-    Console.WriteLine("5. ");
-    Console.Write("Ingrese una opción del menú: ");
+    Console.WriteLine("_____________________________________________________");
+    Console.WriteLine("                    MENU AGREGAR");
+    Console.WriteLine("¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯");
+    Console.WriteLine("1. Agregar paciente");
+    Console.WriteLine("2. Agregar medico");
+    Console.WriteLine("3. Salir");
     int optionSwitch = Convert.ToInt32(Console.ReadLine());
-    return optionSwitch;
-}
-
-void LecturaSwitch()
-{
-    switch (Menu())
+    switch (optionSwitch)
     {
         case 1:
+            //agregarPacinete();
             break;
         case 2:
+            //agregarMedico();
             break;
         case 3:
+            return;
+        default:
+            Console.WriteLine("Ingrese una opción valida"); Console.ReadKey(); break;
+    }
+}
+void consultarListado()
+{
+    Console.Clear();
+    Console.WriteLine("_____________________________________________________");
+    Console.WriteLine("                    MENU LISTADOS");
+    Console.WriteLine("¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯");
+    Console.WriteLine("1. Listado de pacientes");
+    Console.WriteLine("2. Listado de medicos");
+    Console.WriteLine("3. Listado de tratamientos");
+    Console.WriteLine("4. Listado de citas");
+    Console.WriteLine("5. Salir");
+    int optionSwitch = Convert.ToInt32(Console.ReadLine());
+    switch (optionSwitch)
+    {
+        case 1:
+            //listadoPacientes
+            break;
+        case 2:
+            //listadoMedicos
+
+            break;
+        case 3:
+            //listadoTratamientos
             break;
         case 4:
+            //listadoCitas
             break;
         case 5:
-            break;
+            return;
         default:
-            Console.ReadKey();
+            Console.WriteLine("Ingrese una opción valida"); Console.ReadKey();
             break;
     }
 }
-
-
