41d20edf4a3e60048351ea519d98b931e4606b4c
Mily Matías <158805363+Matiasmily@users.noreply.github.com>
2024-08-22 20:40:21 -0600

    se añadieron atributos y el constructos de citas medicas

--- Grupal/Cita medica.cs
+++ Grupal/Cita medica.cs
@@ -6,7 +6,24 @@ using System.Threading.Tasks;
 
 namespace Grupal
 {
-    internal class Cita_medica
+    public class Cita_medica
     {
+
+        protected int NumeroCita {  get; set; }
+        protected DateTime Fecha { get; set; }
+        public Medico Medico { get; set; }
+        public Paciente Paciente { get; set; }
+        protected string Diagnostico { get; set; }
+        protected List<Tratamiento> Tratamientos { get; set; }
+
+        public Cita_medica(int numeroCita, DateTime fecha, Medico medico, Paciente paciente, string diagnostico, List<Tratamiento> tratamientos)
+        {
+            NumeroCita = numeroCita;
+            Fecha = fecha;
+            Medico = medico;
+            Paciente = paciente;
+            Diagnostico = diagnostico;
+            Tratamientos = tratamientos;
+        }
     }
 }
