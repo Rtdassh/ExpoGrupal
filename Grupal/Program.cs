7ddf8f9e72a15120f03c9621ff343319028fcb9f
rtdassh <gonz.deron.gt@gmail.com>
2024-08-22 19:53:09 -0600

    Estructura básica del programa

--- Grupal/Program.cs
+++ Grupal/Program.cs
@@ -1,2 +1,50 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿bool menu = true;
+
+do
+{
+    try
+    {
+        LecturaSwitch();
+    }
+    catch (FormatException ex)
+    {
+        Console.WriteLine("ERROR!, Datos Inválidos. " + ex.Message);
+        Console.ReadKey();
+    }
+} while (menu);
+
+int Menu()
+{
+    Console.Clear();
+    Console.WriteLine("Hospital Pediatrico Alegría");
+    Console.WriteLine("1. ");
+    Console.WriteLine("2. ");
+    Console.WriteLine("3. ");
+    Console.WriteLine("4. ");
+    Console.WriteLine("5. ");
+    Console.Write("Ingrese una opción del menú: ");
+    int optionSwitch = Convert.ToInt32(Console.ReadLine());
+    return optionSwitch;
+}
+
+void LecturaSwitch()
+{
+    switch (Menu())
+    {
+        case 1:
+            break;
+        case 2:
+            break;
+        case 3:
+            break;
+        case 4:
+            break;
+        case 5:
+            break;
+        default:
+            Console.ReadKey();
+            break;
+    }
+}
+
+
